db0deb9ea6befd0e2e650a81dbed12dba45dd576
suibin hong <suibin123@hotmail.com>
2018-09-11 23:31:59 +0100

    final

--- try.js
+++ try.js
@@ -36,9 +36,6 @@ window.onload = function () {
 	
 	var fragment3 = create('<div style="background:#fff5d2;padding:10px 20px;border:1px solid #e3df74; font-size:14px; color:#2c2c2c; font-weight:bold;-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; margin:10px 0px 20px 0px">Your order is reserved for <span id="time"></span> minutes!</div>');
     document.getElementsByClassName('main__header')[0].appendChild(fragment3);
-    
-    var fragment2 = create('<img src="https://www.conversionpirate.com/safesite.jpg" alt="safe site" class="text-center center-block" style="margin:29px 0px">');
-	document.getElementsByClassName('main__header')[0].appendChild(fragment2);
 
 	var ten = 60 * 10,
 	display = document.querySelector('#time');
